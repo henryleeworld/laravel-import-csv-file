5817f7c49cf643e3d4be79f235722b7cbbf5ccf0
Henry Lee <henryleeworld@gmail.com>
2024-10-03 00:24:22 +0800

    feat:升級至 laravel 11

--- database/migrations/2021_09_17_151911_create_csv_data_table.php
+++ database/migrations/2021_09_17_151911_create_csv_data_table.php
@@ -11,14 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
+        Schema::create('csv_data', function (Blueprint $table) {
             $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+            $table->string('csv_filename');
+            $table->boolean('csv_header')->default(0);
+            $table->json('csv_data');
+            $table->timestamps();
         });
     }
 
@@ -27,6 +25,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::table('', function (Blueprint $table) {
+            //
+        });
     }
 };
--- routes/web.php
+++ routes/web.php
@@ -5,16 +5,6 @@ use App\Http\Controllers\ImportController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 Route::middleware('auth')->group(function () {
     Route::get('/', [ContactController::class, 'index'])->name('contacts.index');
     Route::post('/import_parse', [ImportController::class, 'parseImport'])->name('import_parse');
